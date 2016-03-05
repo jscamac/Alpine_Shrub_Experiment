7692e5322972b7e989d9b64d3ceafe98800e84bd
James Camac <james.camac@gmail.com>
2016-03-05 15:11:06 +1100

    Simplified some of the plot themes, decreased font size and tweaked a few plots to ensure it printed correctly

--- R/figures.R
+++ R/figures.R
@@ -2,28 +2,19 @@
 # Theme for partial plots
 partial_plot_theme <- function(legend.position = "none", strips = FALSE,...) {
   sb <- if(strips==TRUE) element_rect(fill='lightgrey') else element_blank()
-  st <- if(strips==TRUE) element_text(color = "black", face = "bold", size = 10) else element_blank()
-  theme_classic() + theme(axis.title = element_text(face = "bold", size = 12),
-                          axis.text = element_text(size = 10),
-                          strip.text = st,
-                          legend.title = element_text(size = 10),
-                          legend.text = element_text(size = 10), 
-                          plot.title = element_text(size = 18),
+  st <- if(strips==TRUE) element_text(face='italic') else element_blank()
+  theme_classic(base_size = 10) + theme(strip.text = st,
                           legend.title = element_blank(),
                           strip.background = sb,
                           legend.position = legend.position,
-                          panel.margin = unit(4,"mm"))
+                          plot.margin = unit(c(5,5,5,5), "mm"))
 }
 
 # Theme for coefficient plots
 coefficent_plot_theme <- function() {
-  theme_classic() + theme(axis.title.x = element_text(face = "bold", size = 12),
+  theme_classic(base_size = 10) + theme(
                           axis.title.y = element_blank(),
-                          axis.text.x = element_text(size = 10),
-                          axis.text.y = element_text(size = 12),
-                          plot.title = element_text(size = 18),
-                          title = element_text(face = "bold"),
-                          panel.margin = unit(4,"mm"))
+                          plot.margin = unit(c(5,5,5,5), "mm"))
 }
 
 
@@ -34,14 +25,16 @@ plot_obs_growth <- function(observed_data, response ='ht', scales ='free', ncol=
                             ylim = c(0,50)) {
   
   summary_obs <- summarise_otc_growth_obs(observed_data, response)
+  summary_obs$year <- as.numeric(year(summary_obs$date))
   observed_data$otc <- factor(ifelse(observed_data$otc==1, 'otc','ctl'))
-    
+  observed_data$year <- as.numeric(year(observed_data$date))
   ggplot() + 
-  geom_path(data = observed_data, aes_string(x = 'date',y = response, colour='otc', group = 'ind'), alpha = 0.4) +
-  geom_path(data = summary_obs, aes(x=date, y=mean, col=factor(paste0(otc, '_mean'))), size=1, alpha=0.8) +
-  geom_pointrange(data= summary_obs,aes(x= date, y=mean,ymin = lower_95ci, ymax=upper_95ci, col=factor(paste0(otc, '_mean'))),size=0.5) +
+  geom_path(data = observed_data, aes_string(x = 'year',y = response, colour='otc', group = 'ind'), alpha = 0.4) +
+  geom_path(data = summary_obs, aes(x=year, y=mean, col=factor(paste0(otc, '_mean'))), alpha=0.6) +
+  geom_pointrange(data= summary_obs,aes(x=year, y=mean,ymin = lower_95ci, ymax=upper_95ci, col=factor(paste0(otc, '_mean'))), size = 0.05) +
   scale_colour_manual('', values = c("ctl" ="sky blue","otc" ="orange", "otc_mean"="red", "ctl_mean"="blue")) +
-    partial_plot_theme(strips = TRUE) + 
+    partial_plot_theme(strips = TRUE) +
+    scale_x_continuous(breaks = c(2010,2012,2014), limits=c(2010,2015)) +
     ylim(ylim) +
     ylab(ylab) + 
     xlab(xlab) + 
@@ -53,10 +46,10 @@ coefficient_plot <- function(summarised_coefficients, y_axis_labels='',xlab ='Ef
   
   
   ggplot(summarised_coefficients, aes(x = mean,y = parameter)) + 
-    geom_segment(aes(x=`2.5%`,y=parameter, xend=`97.5%`, yend=parameter), size=0.5)+
-    geom_segment(aes(x=`10%`,y=parameter, xend=`90%`, yend=parameter), size=1)+
-    geom_point(aes(x=mean, y=parameter), size =3) +
-    geom_vline(aes(x=0), linetype=2) +
+    geom_segment(aes(x=`2.5%`,y=parameter, xend=`97.5%`, yend=parameter), size=0.3)+
+    geom_segment(aes(x=`10%`,y=parameter, xend=`90%`, yend=parameter), size = 0.6)+
+    geom_point(aes(x=mean, y=parameter)) +
+    geom_vline(aes(xintercept=0), linetype=2) +
     scale_y_discrete(labels =rev(y_axis_labels)) +
     xlab(xlab) +
     scale_x_continuous(breaks= scales::pretty_breaks(6)) +
@@ -66,7 +59,7 @@ coefficient_plot <- function(summarised_coefficients, y_axis_labels='',xlab ='Ef
 
 
 # Plot predicted height growth
-plot_growth_curves <- function(non_tussock_otc_growth_model, scales ='free', ncol=4, ylab = 'Predicted height (cm)', xlab ='Year') {
+plot_growth_curves <- function(non_tussock_otc_growth_model, scales ='free', ncol=4, ylab = 'Projected height (cm)', xlab ='Year') {
   predictions <- summarise_otc_model_predictions('non_tussock_growth',non_tussock_otc_growth_model)
   vert_line <- extract_hmax_year(predictions)
   
@@ -87,7 +80,7 @@ plot_growth_curves <- function(non_tussock_otc_growth_model, scales ='free', nco
 # Predictive partial plots for tussock seedlings
 poadist_plot <- function(summarised_predictions, xlab=NULL,ylab=NULL) {
   ggplot(summarised_predictions, aes(x = sim_median_poadist,y = mean, group=as.factor(treatment), colour = as.factor(treatment), fill = as.factor(treatment)), axis.line = element_line()) + 
-    geom_line(size =1.2) +
+    geom_line() +
     scale_colour_manual("",labels = c("Control", "Open Top Chamber"),values= c('ctl' ='blue','otc' ='red')) +
     scale_fill_manual("",labels = c("Control", "Open Top Chamber"),values= c('blue','red')) +
     geom_ribbon(aes(ymin = `2.5%`,ymax = `97.5%`), alpha=0.4, colour=NA) + 
@@ -103,23 +96,22 @@ non_tussock_mortality_plot <- function(non_tussock_mortality_model, ylab ='Annua
   
   
   ggplot(mortality, aes(x = spp,y = mean, group= treatment, colour=treatment)) + 
-    geom_point(position = position_dodge(.1), size=1) +
-    geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`),size=1, position=position_dodge(.1)) +
+    geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`), position=position_dodge(.1)) +
     scale_colour_manual('',values = c("ctl" ="blue","otc" ="red")) +
     ylab(ylab) + 
     xlab(xlab) +
-    partial_plot_theme()
+    partial_plot_theme() + 
+    theme(axis.text.x = element_text(face='italic'))
 }
 
 # Predicted number of natural recruits in OTC vs CTL for first two seasons
-recruit_plot <- function(recruit_model, ylab =expression(bold('Recruits/'~m^2)), xlab ='Census') {
+recruit_plot <- function(recruit_model, ylab =expression('Recruits/'~m^2), xlab ='Census') {
   recruits <- summarise_otc_model_predictions('recruits',recruit_model)
   recruits$census <- as.factor(rep(c('2011', '2012'),2))
   
   
   ggplot(recruits, aes(x = census,y = mean, group= treatment, colour=treatment)) + 
-    geom_point(position = position_dodge(.1), size=1) +
-    geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`),size=1, position=position_dodge(.1)) +
+    geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`), position=position_dodge(.1)) +
     scale_colour_manual('',values = c("ctl" ="blue","otc" ="red")) +
     ylab(ylab) + 
     xlab(xlab) +
@@ -130,7 +122,7 @@ recruit_plot <- function(recruit_model, ylab =expression(bold('Recruits/'~m^2)),
 # Plots covariate partial plots for density and height models
 partial_plot_density_height <- function(predictions, x, xlab=NULL,ylab=NULL, ylim =c(0,15)) {
   ggplot(predictions, aes_string(x = x,y = 'mean'), axis.line = element_line()) + 
-    geom_line(size =1.2) +
+    geom_line() +
     scale_fill_manual("",values= 'grey60') +
     geom_ribbon(aes(ymin = `2.5%`,ymax = `97.5%`), alpha=0.4, colour=NA) + 
     scale_x_continuous(expand=c(0,0.001)) +
@@ -142,17 +134,16 @@ partial_plot_density_height <- function(predictions, x, xlab=NULL,ylab=NULL, yli
 
 burnt_v_unburnt <- function(predictions, x, ylab=NULL) {
   ggplot(predictions, aes_string(x = x,y = 'mean')) + 
-    geom_point(size=1) +
-    geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`),size=1) +
+    geom_pointrange(aes(ymin = `2.5%`, ymax=`97.5%`), size=0.3) +
     scale_x_discrete(labels =c('Unburnt','Burnt')) +
     xlab('') +
     ylab(ylab) +
     partial_plot_theme()
 }
 
-gap_dynamics_curve <- function(summarised_predictions, xlab='Year',ylab='Inter-tussock gap radius (cm)',ylim=c(0,25)) {
+gap_dynamics_curve <- function(summarised_predictions, xlab='Year',ylab='Inter-tussock gap radius (cm)',ylim=c(0,15)) {
   ggplot(summarised_predictions, aes(x = pred_yr,y = mean, group=as.factor(treatment), colour = as.factor(treatment), fill = as.factor(treatment)), axis.line = element_line()) + 
-    geom_line(size =1.2) +
+    geom_line() +
     scale_colour_manual("",labels = c("Control", "Open Top Chamber"),values= c('ctl' ='blue','otc' ='red')) +
     scale_fill_manual("",labels = c("Control", "Open Top Chamber"),values= c('blue','red')) +
     geom_ribbon(aes(ymin = `2.5%`,ymax = `97.5%`), alpha=0.4, colour=NA) + 
@@ -169,23 +160,23 @@ gap_dynamics_curve <- function(summarised_predictions, xlab='Year',ylab='Inter-t
 obs_pred_ht_growth <- function(observed_data, non_tussock_otc_growth_model) {
   p1 <- plot_obs_growth(observed_data,'ht')
   p2 <- plot_growth_curves(non_tussock_otc_growth_model)
-  grid.arrange(p1, p2, ncol=1)
+  print(plot_grid(p1,p2, labels=letters[1:2], ncol = 1, label_size = 11),vp=viewport(layout.pos.row = 1, layout.pos.col = 1))
 }
-
+  
 # Partial plots for growth and mortality against poa distance size
 tussock_plots <- function(tussock_growth_model, tussock_mortality_model) {
   growth <- summarise_coefficients(tussock_growth_model, c('alpha','b_otc','b_poadist','b_otc_x_poadist'))
   mortality <- summarise_coefficients(tussock_mortality_model, c('alpha','b_otc','b_poadist','b_otc_x_poadist'))
   growth_preds <- summarise_otc_model_predictions('tussock_growth',tussock_growth_model)
   mortality_preds <- summarise_otc_model_predictions('tussock_mortality',tussock_mortality_model)
-  p1 <- coefficient_plot(growth, y_axis_labels = c('intercept','otc','gap','otc x gap'), 
+  p1 <- coefficient_plot(growth, y_axis_labels = c('intercept','warmed','gap','warmed x gap'), 
                          xlab = 'Growth coefficients')
   p2 <- poadist_plot(growth_preds,xlab = 'Inter-tussock gap radius (cm)',ylab = 'logistic growth rate parameter (R)')
-  p3 <- coefficient_plot(mortality, y_axis_labels = c('intercept','otc','gap','otc x gap'),
+  p3 <- coefficient_plot(mortality, y_axis_labels = c('intercept','warmed','gap','warmed x gap'),
                          xlab = 'Mortality coefficents (cloglog scale)')
   
   p4 <- poadist_plot(mortality_preds, xlab = 'Inter-tussock gap radius (cm)',ylab = 'Annual probability of death')
-  grid.arrange(p1,p2,p3,p4, ncol=2)
+  plot_grid(p1,p2,p3,p4, labels=letters[1:4], ncol = 2, label_size = 11)
 }
 
 density_count_plots <- function(density_model, species, ylim=c(0,32)) {
@@ -200,21 +191,22 @@ density_count_plots <- function(density_model, species, ylim=c(0,32)) {
   coeffs <- summarise_coefficients(density_model,params = params)
   predictions <- summarise_density_predictions(density_model,species)
   p1 <- coefficient_plot(coeffs,y_axis_labels,xlab = 'log coefficients')
-  p2 <- burnt_v_unburnt(predictions$burnt_unburnt,x ='predictor',ylab = expression(bold('Seedlings /'~m^2)))
+  p2 <- burnt_v_unburnt(predictions$burnt_unburnt,x ='predictor',ylab = expression('Seedlings /'~m^2))
   p3 <- partial_plot_density_height(predictions$severity, x ='sim_severity', 
-                                    xlab ='Minimum Twig diameter (mm)', ylab =expression(bold('Seedlings /'~m^2)),ylim)
+                                    xlab ='Minimum Twig diameter (mm)', ylab =expression('Seedlings /'~m^2),ylim)
   p4 <- partial_plot_density_height(predictions$altitude, x ='sim_altitude', 
-                                    xlab ='Altitude (m)', ylab =expression(bold('Seedlings /'~m^2)),ylim) 
+                                    xlab ='Altitude (m)', ylab =expression('Seedlings /'~m^2),ylim) 
   p5 <- partial_plot_density_height(predictions$twi, x ='sim_twi', 
-                                    xlab ='Topographic wetness index', ylab =expression(bold('Seedlings /'~m^2)),ylim)
+                                    xlab ='Topographic wetness index', ylab =expression('Seedlings /'~m^2),ylim)
   
   if(species=='Grevillea') {
     p6 <- partial_plot_density_height(predictions$adult_density, x ='sim_adult_den', 
-                                      xlab = expression(bold('Adult density/'~m^2)), ylab = expression(bold('Seedlings /'~m^2)), ylim)
-    grid.arrange(p1,p2,p3,p4,p5,p6, ncol=2)
+                                      xlab = expression('Adult density/'~m^2), ylab = expression('Seedlings /'~m^2), ylim)
+    plot_grid(p1,p2,p3,p4,p5,p6, labels=letters[1:6], ncol = 2, label_size = 11)
   }
-  if(species=='Asterolasia') {
-    grid.arrange(p1,p2,p3,p4,p5, ncol=2)
+  
+  else{
+      plot_grid(p1,p2,p3,p4,p5, labels=letters[1:5], ncol = 2, label_size = 11)
   }
 }
 
@@ -227,8 +219,8 @@ max_ht_plots <- function(greaus_max_ht_model,asttry_max_ht_model, ylim=c(0,35)) 
   coeffs_asttry <- summarise_coefficients(asttry_max_ht_model,params = c('mu_log_site','log_b_severity','log_b_altitude','log_b_twi'))
   predictions_asttry <- summarise_max_ht_predictions(asttry_max_ht_model)
   
-  p1 <- coefficient_plot(coeffs_asttry,c('Intercept','severity','altitude','twi'),xlab = 'log coefficients', title='Asterolasia')
-  p2 <- coefficient_plot(coeffs_greaus,c('Intercept','severity','altitude','twi'),xlab = 'log coefficients', title='Grevillea')
+  p1 <- coefficient_plot(coeffs_asttry,c('Intercept','severity','altitude','twi'),xlab = 'log coefficients', title=expression(italic('Asterolasia')))
+  p2 <- coefficient_plot(coeffs_greaus,c('Intercept','severity','altitude','twi'),xlab = 'log coefficients', title=expression(italic('Grevillea')))
   p3 <- partial_plot_density_height(predictions_asttry$pred_ht_severity, x ='sim_severity', 
                                     xlab ='Minimum Twig diameter (mm)', ylab ='Maximum height (cm)',ylim)
   p4 <- partial_plot_density_height(predictions_greaus$pred_ht_severity, x ='sim_severity', 
@@ -241,8 +233,7 @@ max_ht_plots <- function(greaus_max_ht_model,asttry_max_ht_model, ylim=c(0,35)) 
                                     xlab ='Topographic wetness index', ylab ='Maximum height (cm)',ylim)
   p8 <- partial_plot_density_height(predictions_greaus$pred_ht_twi, x ='sim_twi', 
                                     xlab ='Topographic wetness index', ylab ='Maximum height (cm)',ylim)
-  
-  grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8, ncol=2)
+  plot_grid(p1,p2,p3,p4,p5,p6,p7,p8, labels=letters[1:8], ncol = 2, label_size = 11)
 }
 
 gap_dynamics_plot <- function(gap_dynamic_model) {
@@ -250,7 +241,7 @@ gap_dynamics_plot <- function(gap_dynamic_model) {
   predictions <- summarise_otc_model_predictions('gap_dynamics',gap_dynamic_model)
   p1 <- coefficient_plot(coeffs,y_axis_labels = c('Intercept', 'otc'), xlab = 'log coefficients')
   p2 <- gap_dynamics_curve(predictions)
-    grid.arrange(p1,p2, ncol=2)
+  plot_grid(p1,p2, labels=letters[1:2], ncol = 2, label_size = 11)
 }
 
 examine_plot_microclimates <- function(hourly_microclimate, 
@@ -304,11 +295,12 @@ plot_microclim_trt_diff <- function(hourly_microclimate,
   
   ggplot(dat, aes(x = date,y = difference), axis.line = element_line()) + 
     geom_rect(data = chamber_dates, aes(xmin = chambers_in, xmax = chambers_out, 
-                                        ymin = -Inf, ymax = Inf), alpha = 0.2, inherit.aes=FALSE) + 
+                                        ymin = -Inf, ymax = Inf), alpha = 0.2, inherit.aes=FALSE, na.rm=TRUE) + 
     geom_path() +
     geom_hline(data = mean_diff, aes(yintercept = mean), col='red', alpha=0.6, size = 1) +
     geom_hline(aes(yintercept = 0), col='blue', alpha=0.6, size= 1) +
     scale_x_date(expand=c(0,0), limits =c(min_date,max_date)) +
+    xlab('Year') +
     ylab(ylab) +
     partial_plot_theme(strips = FALSE)
 }
@@ -317,25 +309,25 @@ microclimate_diff_plots <- function(hourly_microclimate, subset_site='ITEX2.0', 
   p1 <- plot_microclim_trt_diff(hourly_microclimate, 
                                 subset_sensor = 'CAmbient_Temp',
                                 stat = stat,
-                                ylab = substitute(bold(stat_label~'ambient temperature'~paste('(',degree, C,')')),list(stat_label=stat_label)))
+                                ylab = substitute(stat_label~'ambient temperature'~paste('(',degree, C,')'),list(stat_label=stat_label)))
   
   p2 <- plot_microclim_trt_diff(hourly_microclimate, 
                                 subset_sensor = 'CTemp_3cmBG',
                                 stat = stat,
-                                ylab = substitute(bold(stat_label~'soil temperature'~paste('(',degree, C,')')),list(stat_label=stat_label)))
+                                ylab = substitute(stat_label~'soil temperature'~paste('(',degree, C,')'),list(stat_label=stat_label)))
   
   p3 <- plot_microclim_trt_diff(hourly_microclimate, 
                                 subset_sensor = 'CRH',
                                 stat = stat,
-                                ylab = substitute(bold(stat_label~'relative humidity (%)'),list(stat_label=stat_label)))
+                                ylab = substitute(stat_label~'relative humidity (%)',list(stat_label=stat_label)))
     
 
   p4 <- plot_microclim_trt_diff(hourly_microclimate, 
                                 subset_sensor = 'CMoisture3to10cmBG',
                                 stat = stat,
-                                ylab = substitute(bold(stat_label~'volumetric water content (%)'),list(stat_label=stat_label)))
+                                ylab = substitute(stat_label~'volumetric water content (%)',list(stat_label=stat_label)))
   
-  grid.arrange(p1,p2,p3,p4, ncol=2)
+  plot_grid(p1,p2,p3,p4, labels=letters[1:4], ncol = 2, label_size = 11)
       
 }
 
